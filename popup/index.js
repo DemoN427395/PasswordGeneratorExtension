96643bcdfc163a53287515169061c7f7f75ca866
DemoN <rudoffya@yandex.ru>
2023-12-30 03:37:34 +0300

    Version 2.0.0

--- popup/index.js
+++ popup/index.js
@@ -1,21 +1,29 @@
 window.onload = function() {
 
     let inputArea = document.querySelector("#inputArea");
+    let slider = document.getElementById("myRange");
+    let output = document.getElementById("output");
 
-    // let inputLength = document.querySelector("#inputLength");
+    output.innerHTML = slider.value;
+
+    slider.oninput = function() {
+        output.innerHTML = this.value;
+    }
+
 
     var generatePassword = (
-        length = 20,
+        length = output.innerHTML,
         characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~!@-#$'
     ) =>
         Array.from(crypto.getRandomValues(new Uint32Array(length)))
             .map((x) => characters[x % characters.length])
             .join('')
 
+            console.log(generatePassword());
+            
     document.getElementById('generate').addEventListener('click', function() {
         let password = generatePassword();
         inputArea.value = password;
-        console.log(password);
     });
 
     document.getElementById('copy').addEventListener('click', function() {
